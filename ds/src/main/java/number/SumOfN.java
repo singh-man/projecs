d240064d394cc187f5ac761290411c7b4c32470b
Manish Singh <prataponandroid@gmail.com>
2022-06-17 15:46:48 +0100

    more ds added

--- ds/src/main/java/number/SumOfN.java
+++ ds/src/main/java/number/SumOfN.java
@@ -3,9 +3,6 @@ package number;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class SumOfN {
@@ -16,10 +13,10 @@ public class SumOfN {
     }
 
     // Functional Style Recursion
-    final Function<Integer, Integer> sumN = i -> i == 0 ? i : i + this.sumN.apply(i -1);
+    final Function<Integer, Integer> sumN = i -> i == 0 ? i : i + this.sumN.apply(i - 1);
 
-    private int sumOfNConstTime(int num) {
-        return ((1+num)*num)/2;
+    private long sumOfNConstTime(long num) {
+        return ((1 + num) * num) / 2;
     }
 
     @Test
@@ -27,5 +24,10 @@ public class SumOfN {
         Assert.assertEquals(55, sumOfN(10));
         Assert.assertEquals(Integer.valueOf(55), sumN.apply(10));
         Assert.assertEquals(sumOfN(10), sumOfNConstTime(10));
+    }
+
+    @Test
+    public void testSumOfNConst() {
+        System.out.println(sumOfNConstTime(Integer.MAX_VALUE));
     }
 }
